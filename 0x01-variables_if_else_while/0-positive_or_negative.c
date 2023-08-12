c29dce4f1ffb3c00613b79a02e6da5e87375daca
tega <dajakpome.t@gmail.com>
2023-08-12 12:13:45 -0700

    variable

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,11 +16,11 @@ int main(void)
     printf("The number: %d\n", n);
 
     if (n > 0) {
-        printf("is positive\n");
+        printf("is positive\n",	 n);
     } else if (n == 0) {
-        printf("is zero\n");
+        printf("is zero\n",	n);
     } else {
-        printf("is negative\n");
+        printf("is negative\n",	n);
     }
     return 0;
 }
