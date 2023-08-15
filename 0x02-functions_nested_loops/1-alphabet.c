eb338864cf927997115148aa068ee1311d3d6f95
Tega812 <dajakpome.t@gmail.com>
2023-08-15 14:20:39 +0100

    alphabet.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function of alphabet
+ * Description - this is the program
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	putchar ('\n');
+}
